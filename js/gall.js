1843e206aee8532dfab8642ed69cec525a6b8e7d
Malcolm Young <malcolm.young@capgemini.com>
2016-04-26 22:57:23 +0100

    hover transition on tiles

--- js/gall.js
+++ js/gall.js
@@ -1,13 +1,16 @@
 (function ($, Drupal) {
   /**
-   * Initialise the tabs JS.
+   * Set up hover classes on tiles.
    */
   Drupal.behaviors.gallTiles = {
     attach: function (context, settings) {
       var $tiles = $(context).find('.view-galleries-a-z .views-row a');
       if ($tiles.length) {
-        $tiles.on('focus blur', function() {
-          $(this).closest('.views-row').toggleClass('has-focus');
+        $tiles.on('focus mouseenter', function() {
+          $('.views-row.has-focus').removeClass('has-focus');
+          $(this).closest('.views-row').addClass('has-focus');
+        }).on('blur mouseleave', function() {
+          $(this).closest('.views-row').removeClass('has-focus');
         });
       }
     }
