fb525e5cc04c577c7a9ebb570b1bad261882f94e
Malcolm Young <428782+malcomio@users.noreply.github.com>
2026-09-13 08:47:53 +0100

    Dart sass (#111)
    * Drupal 10 compatibility
    
    * gulpfile for dart-sass
    
    * basic functionality for dart-sass
    
    * handle icons
    
    * remove browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,32 @@
-const {src, dest, watch, series, parallel} = require('gulp');
-const gulp = require('gulp');
+const { src, dest, watch, series } = require("gulp");
+const plumber = require("gulp-plumber");
+const $ = require("gulp-load-plugins")();
+const sass = require("gulp-sass")(require("sass"));
 
-var sass = require('gulp-sass');
+/**
+ * Compile Sass to CSS.
+ *
+ * @return {*} A stream.
+ */
+const buildStyles = () =>
+  src("sass/gall.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(dest("css/"));
 
-sass.compiler = require('node-sass');
-
-const
-  $ = require('gulp-load-plugins')(),
-  browserSync = require('browser-sync').create(),
-  reload = browserSync.reload,
-  minimist = require('minimist'),
-  arg = minimist(process.argv.slice(2)),
-  svgmin = require('gulp-svgmin'),
-  svgstore = require('gulp-svgstore'),
-  cheerio = require('gulp-cheerio');
-
-// Default environment options.
-var envOption = {
-  minify: false,
-  sourcemap: true,
-  styleGuide: true
-};
-
-// Set environment options. Vagrant uses defaults.
-switch (arg.env) {
-  case 'production':
-    envOption.minify = true;
-    envOption.sourcemap = false;
-    envOption.styleGuide = false;
-    break;
-}
-
-// File paths
-const files = {
-  scssPath: './sass/**/*.scss',
-  iconPath: './icons/*'
-};
-
-gulp.task('icons', function () {
-  return gulp.src('./src/icons/*')
-    .pipe(svgmin())
-    .pipe(svgstore({ fileName: 'icons.svg', inlineSvg: true}))
-    .pipe(cheerio({
+const icons = () =>
+  src("icons/*.svg")
+    .pipe($.svgmin())
+    .pipe($.svgstore({ fileName: 'icons.svg', inlineSvg: true }))
+    .pipe($.cheerio({
       run: function ($, file) {
         $('svg').addClass('hidden');
         $('[fill]').removeAttr('fill');
       },
       parserOptions: { xmlMode: true }
     }))
-    .pipe(gulp.dest('./images/'));
-});
+    .pipe(dest('./images/'));;
 
-gulp.task('browser-sync', function(done) {
-  browserSync.init({
-    proxy: 'thegalleryguide.lndo.site/',
-    notify: false
-  });
-
-  browserSync.watch('./templates/**').on('change', browserSync.reload);
-
-  browserSync.watch('./css/**').on('change', browserSync.reload);
-
-  done()
-});
-
-gulp.task('sass-lint', function () {
-  return gulp.src('sass/**/*.scss')
-    .pipe($.cached($.scssLint))
-    .pipe($.scssLint({
-      'config': 'scss-lint.yml'
-    }));
-});
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-
-        .pipe($.if(envOption.sourcemap, $.sourcemaps.init()))
-        .pipe($.sass())
-        .pipe($.autoprefixer('last 2 versions', 'ie 8', 'ie 9'))
-        // Optionally produce production CSS.
-        .pipe($.if(envOption.sourcemap, $.sourcemaps.write('./')))
-        .pipe($.if(envOption.minify, $.minifyCss()))
-        .pipe(dest('css'))
-
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('build', gulp.series('sass'));
-
-gulp.task('watch', gulp.series('sass', 'browser-sync', function(done) {
-  gulp.watch('./sass/**/*.*', gulp.series('sass', 'sass-lint'));
-  done()
-}));
-
-gulp.task('default', gulp.series('sass', 'sass-lint', 'watch'));
+exports.icons = icons;
+exports.styles = series(buildStyles);
+exports.build = series(icons, buildStyles);
